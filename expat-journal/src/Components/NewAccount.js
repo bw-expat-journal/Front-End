a923b6b487bfbbccb8e68e36bbb5b090ff78c6be
Kenya-a <Kenya-a@users.noreply.github.com>
2019-07-29 17:46:22 -0500

    added styling

--- expat-journal/src/Components/NewAccount.js
+++ expat-journal/src/Components/NewAccount.js
@@ -1,24 +1,7 @@
 import React, { useState }from 'react';
-//import signUpForm from './customHooks'
-
-
-// const {inputs, handleChange, handleSubmit} = signUpForm(signUP);
-
-// const signUP = () => {
-//     alert(`New User !! Name: ${inputs.firstName} ${inputs.lastName}
-//     Email: ${inputs.email}`)
-// }
+import styled from 'styled-components';
 
 const NewAccount = () => {
-
-
-//     const {inputs, handleChange, handleSubmit} = signUpForm(signUP);
-
-// const signUP = () => {
-//     alert(`New User !! Name: ${inputs.firstName} ${inputs.lastName}
-//     Email: ${inputs.email}`)
-// }
-
 
     const [inputs, setInputs] = useState({ firstName: '', lastName: '',password0:'', password1:''});
 
@@ -38,29 +21,73 @@ const NewAccount = () => {
 
     return (
 
-        <form onSubmit = {handleSubmit}>
-            <div>
+        <Form onSubmit = {handleSubmit}>
+            <Legend>Create an Account</Legend>
+           <div>
+            <Inputs>
                 <label>First Name</label>
-                <input type = 'text' name = 'firstName' onChange = {handleChange} value = {inputs.firstName} required/>
+                <Inputt type = 'text' name = 'firstName' onChange = {handleChange} value = {inputs.firstName} required/>
+            </Inputs>
+            <Inputs>
                 <label>Last Name</label>
-                <input type = 'text' name = 'lastName' onChange = {handleChange} value = {inputs.lastName} required/>
-            </div>
-            <div>
+                <Inputt type = 'text' name = 'lastName' onChange = {handleChange} value = {inputs.lastName} required/>
+            </Inputs>
+            <Inputs>
             <label>Enter Your Email</label>
-                <input type = 'email' name = 'email' onChange = {handleChange} value = {inputs.email} required/>
-            </div>
-            <div>
+                <Inputt type = 'email' name = 'email' onChange = {handleChange} value = {inputs.email} required/>
+            </Inputs>
+            <Inputs>
             <label>Password</label>
-                <input type = 'password' name = 'password0' onChange = {handleChange} value = {inputs.password0} required/>
-            </div>
-            <div>
+                <Inputt type = 'password' name = 'password0' onChange = {handleChange} value = {inputs.password0} required/>
+            </Inputs>
+            <Inputs>
             <label>Re-enter Password</label>
-                <input type = 'password' name = 'password1' onChange = {handleChange} value = {inputs.password1} required/>
+                <Inputt type = 'password' name = 'password1' onChange = {handleChange} value = {inputs.password1} required/>
+            </Inputs>
             </div>
-            <button type = 'submit' >Sign Up</button>
-        </form>
+            <Button type = 'submit' >Sign Up</Button>
+        </Form>
     )
 }
 
 export default NewAccount;
 
+///Styled Components
+
+const Form = styled.form`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+background: #c9e5eb
+padding: 5px;
+margin:5px;
+border-radius: 8px;
+color: #1f4852;
+font-size: 20px;
+
+`
+const Legend = styled.legend`
+font-size: 28px;
+margin: 5px;
+`
+
+const Button = styled.button`
+border-radius: 8px;
+background-color: white;
+color:blue;
+border: blue;
+padding: 15px 32px;
+`
+const Inputs = styled.div`
+margin: 5px;
+display: flex;
+flex-direction: column;
+`
+
+const Inputt = styled.input`
+border: solid 2px #3c8c9e;
+border-radius: 8px;
+width: 27rem;
+height: 2rem;
+`
