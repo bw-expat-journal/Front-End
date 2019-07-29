3e5f9155fd9eee187dbaf942cbf660d3d4d3cd52
Rosa Perez <rosaperez@Rosas-MacBook-Pro.local>
2019-07-29 14:07:28 -0700

    initial commit

---
+++ expat-journal/src/Components/Login/Login.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import React, { useState } from ‘react’
+import axios from 'axios';
+
+function Login(props) {
+
+    return (
+        <div className='login'>
+        )
+         </div>
+}
+
+
+
+export default Login;
