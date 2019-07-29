6917d58d44ed7940ceb502529d1de5a5afa1ea98
Rosa Perez <rosaperez@Rosas-MacBook-Pro.local>
2019-07-29 14:51:20 -0700

    login rendering

--- expat-journal/src/Components/Login/Login.js
+++ expat-journal/src/Components/Login/Login.js
@@ -1,15 +1,22 @@
-import React, { Component } from 'react';
-import React, { useState } from ‘react’
+import React, { Component, useState } from 'react';
 import axios from 'axios';
 
-function Login(props) {
+export default function Login(props) {
+    const [username, setUsername] = useState({})
+  
 
-    return (
-        <div className='login'>
-        )
-         </div>
+return (
+    <div className="login-form">
+    <div className="container">
+    <form>
+    <label className="label">Email Address</label>
+    <input className="input" type="email" name="email" required />
+    
+    <label className="label">Password</label>
+    <input className="input" type="password" name="password" required />
+
+    </form>
+    </div>
+</div>
+);
 }
-
-
-
-export default Login;
---
+++ expat-journal/src/Components/Login/LoginForm.js
@@ -0,0 +1,21 @@
+// import React from 'react';
+
+// const Form = () => {
+//   return (
+//     <div className="login-form">
+//               <div className="container">
+//               <form>
+//               <label className="label">Email Address</label>
+//               <input className="input" type="email" name="email" required />
+              
+//               <label className="label">Password</label>
+//               <input className="input" type="password" name="password" required />
+
+//               </form>
+//               </div>
+//     </div>
+//   );
+// };
+
+// export default Form;
+
