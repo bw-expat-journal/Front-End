065f08acd2cb07c4c399de4dba6d02e9b3e96be7
Rosa Perez <rosaperez@Rosas-MacBook-Pro.local>
2019-07-30 14:01:19 -0700

    pull from master

--- expat-journal/src/App.js
+++ expat-journal/src/App.js
@@ -3,7 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 import NewAccount from './Components/NewAccount'
 
-// import Login from "../src/Components/Login/Login";
 import Login from "../src/Components/Login"
 
 function App() {
