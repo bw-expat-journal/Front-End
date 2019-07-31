1577bd9a387b79b600e99092feed44c3856d342a
Rosa Perez <rosaperez@Rosas-MacBook-Pro.local>
2019-07-31 14:02:40 -0700

    tabNav

--- expat-journal/src/Components/TabNav.js
+++ expat-journal/src/Components/TabNav.js
@@ -14,7 +14,7 @@ const Nav = props => (
 
 const createLabel = (iconName, labelText) => <span><Icon name={iconName} />{labelText}</span>
 
-const welcomeLabel = createLabel("home", "Home Page")
+const welcomeLabel = createLabel("home", "Home")
 const signUpLabel = createLabel("signup", "Sign-Up")
 const characterLabel = createLabel("chain", "Login")
 // const journalsLabel = createLabel("bolt", "Create Post")
