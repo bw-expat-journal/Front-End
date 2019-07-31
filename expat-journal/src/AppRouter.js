48cba28a47b4d3af51b749899e338cb414d61f4f
Rosa M Perez <rosaperezlarios@gmail.com>
2019-07-31 09:44:49 -0700

    Update AppRouter.js

--- expat-journal/src/AppRouter.js
+++ expat-journal/src/AppRouter.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './Components/Login';
 import NewAccount from './Components/NewAccount';
 import JournalForm from './Components/Journal/JournalForm';
-
-
+import JournalFormCard from './Components/Journal/JournalFormCard';
+import { PostDetails } from './Components/Journal/JournalData';
 export default function AppRouter() {
+    const [list, setList] = useState([...PostDetails])
+const[postEdit, setPostEdit] = useState(null);
+const editPost = post => {
+  const editIndex = list.indexOf(postEdit);
+  setList(list.map((submission, index) => (index === editIndex ? post : submission)))
+}
     return <div className="page-view-ui">
         <Switch>
-            <Route path='/' exact component={NewAccount}/>
+            <Route exact path='/' component={NewAccount}/>
             <Route path='/login' component={Login}/>
-            <Route path='/submit' component={JournalForm}/>
+            <Route 
+              path='/submit' 
+              render={(props) =>
+              <div> 
+              <JournalForm {...props} 
+                list={list} 
+                setList={setList}
+                postEdit={postEdit}
+                setPostEdit={setPostEdit}
+                editPost={editPost}
+              />
+               {list.map((post, index) => {
+               return(
+               <JournalFormCard key={index} post={post} setPostEdit={setPostEdit} />
+               );
+              })};
+              </div>}
+            />
         </Switch>
     </div>
 }
