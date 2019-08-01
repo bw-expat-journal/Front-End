7dfe92ad81a3d899f96e7dbe64dd26e3a92c534d
Kenya-a <Kenya-a@users.noreply.github.com>
2019-08-01 12:21:40 -0500

    removed create a post

--- expat-journal/src/Components/TabNav.js
+++ expat-journal/src/Components/TabNav.js
@@ -17,7 +17,7 @@ const createLabel = (iconName, labelText) => <span><Icon name={iconName} />{labe
 const welcomeLabel = createLabel("home", "Home")
 const signUpLabel = createLabel("signup", "Sign-Up")
 const characterLabel = createLabel("chain", "Login")
- const journalsLabel = createLabel("bolt", "Create Post")
+//  const journalsLabel = createLabel("bolt", "Create Post")
 
 
 
@@ -26,7 +26,7 @@ const panes = [
   { menuItem: <Menu.Item key='sign-up' as={Nav} to={`/sign-up`} content={signUpLabel} /> },
 
   { menuItem: <Menu.Item key='login' as={Nav} to={`/`} content={characterLabel} /> },
- { menuItem: <Menu.Item key='journals' as={Nav} to={`/journals`} content={journalsLabel} /> },
+//  { menuItem: <Menu.Item key='journals' as={Nav} to={`/journals`} content={journalsLabel} /> },
 ]
 
 const TabNav = () => <Tab panes={panes} renderActiveOnly={false} />
